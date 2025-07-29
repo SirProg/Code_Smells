e3b2b45913d215839b4187c5235d5e4d30ef95ec
JuanAyala619 <juaseaya@espol.edu.ec>
2025-07-28 21:32:03 -0500

    Cambio mínimo

--- couplers/feature_envy/Calculo.java
+++ couplers/feature_envy/Calculo.java
@@ -1,5 +1,6 @@
 package feature_envy;
 public class Calculo {
+    double valorImpuesto;
     public double calcularTotal(Pedido pedido) {
         return pedido.getSubtotal() + pedido.getImpuesto();
     }
