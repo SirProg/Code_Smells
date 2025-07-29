deb30a034f3bddd4c31d317dc9c377e38923bff0
IvanAlvarado999 <ivdaalca55@gmail.com>
2025-07-28 22:19:55 -0500

    Add files via upload

---
+++ broaster/primitive_obsession/Usuario.java
@@ -0,0 +1,15 @@
+
+public class Usuario {
+    private String nombre;
+    private String cedula;
+    private String correo;
+    private String contrasena;
+
+    public Usuario(String nombre, String cedula, String correo, String contrasena) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+}
